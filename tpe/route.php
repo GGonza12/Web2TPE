026976c72635120ab55cc2117c99f9a57b2470d9
Gonzalo Ariel Gomez <gonza12ariel@gmail.com>
2021-10-08 19:17:37 -0300

    Empezamos el tpe.

---
+++ tpe/route.php
@@ -0,0 +1,36 @@
+<?php
+require_once ('Controller/StoreController.php');
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+
+// lee la acción
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'home'; // acción por defecto si no envían
+}
+
+$params = explode('/', $action);
+
+$storeController = new StoreController();
+
+// determina que camino seguir según la acción
+switch ($params[0]) {
+    case 'home':
+        var_dump($params);
+        $storeController->showHome(); 
+        break;
+    case 'createGame': 
+        $storeController->createGame(); 
+        break;
+    case 'deleteGame': 
+        $storeController->deleteGame(); 
+        break;
+    case 'viewGame': 
+        $storeController->viewGame(); 
+        break;
+    default: 
+        echo('404 Page not found'); 
+        break;
+}
