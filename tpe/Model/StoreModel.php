81643373a2fa32496020d9db9682e08063686cbc
Juan Luna <xjuanigancio@gmail.com>
2021-10-08 21:00:49 -0300

    Actualizacion del StoreMode.

---
+++ tpe/Model/StoreModel.php
@@ -0,0 +1,33 @@
+<?php
+
+    class StoreModel{
+        private $db;
+            
+        function_contruct(){
+            $this-> db = new PDO('mysql:host=localhost;'.'dbname=db_juegos;charset=utf8', 'root', '');
+        }
+    }
+
+    function GetGames(){
+        $sentencia = $this->db->prepare("SELECT * from juegos");
+        $sentencia->execute();
+        $juegos = $sentencia->fetchAll(PDO::FETCH_OBJ);
+        return $juegos;
+    }
+
+    function CreateGame($juegos,$descripcion,$precio,$id_empresa){
+        $sentencia = $this->db->prepare("INSERT INTO juegos(juego,descripcion,precio,id_empresa) VALUES(?, ?, ?, ?)");
+        $sentencia->execute(array($juegos,$descripcion,$precio,$id_empresa));
+    }
+
+    function DeleteGame($id){
+        $sentencia = $this->db->prepare("DELETE FROM juegos WHERE id_juego");
+        $sentencia = execute(array($id));
+    }
+
+    function ViewGame($id){
+        $sentencia = $this->db->prepare("SELECT * from juegos WHERE id_juego");
+        $sentencia->execute(array($id));
+        $juego = $sentencia->fetch(PDO::FETCH_OBJ);
+        return $juego;
+    }
