8d4b6fc34f6a6b31e1b8ede14f0c2e48cf5a30e1
Gonzalo Ariel Gomez <gonza12ariel@gmail.com>
2021-10-14 17:42:50 -0300

    errores corregidos.

--- tpe/templates_c/7b16d1c2dfcfb2346f10c1987be91ad8ba2b9681_0.file.update.tpl.php
+++ tpe/templates_c/7b16d1c2dfcfb2346f10c1987be91ad8ba2b9681_0.file.update.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-10-13 23:40:14
+/* Smarty version 3.1.39, created on 2021-10-14 22:41:55
   from 'C:\xampp\htdocs\Web2TPE\tpe\templates\update.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6167523eda47f7_80011476',
+  'unifunc' => 'content_61689613aec973_76178005',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7b16d1c2dfcfb2346f10c1987be91ad8ba2b9681' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Web2TPE\\tpe\\templates\\update.tpl',
-      1 => 1634161211,
+      1 => 1634243996,
       2 => 'file',
     ),
   ),
@@ -22,13 +22,16 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_6167523eda47f7_80011476 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61689613aec973_76178005 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
+
 <form action="<?php echo BASE_URL;?>
 UpdateCompany" method="post">
         <input type="text" placeholder="<?php echo $_smarty_tpl->tpl_vars['empresa']->value;?>
 " name="empresa" >
+        <input type="text" placeholder="<?php echo $_smarty_tpl->tpl_vars['descripcion']->value;?>
+" name="descripcion" >
         <input hidden type="text"  value="<?php echo $_smarty_tpl->tpl_vars['id']->value;?>
 " name="id">
         <input type="submit" value="Modificar">
